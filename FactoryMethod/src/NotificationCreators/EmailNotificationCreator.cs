56f9b1be9bfc083ec8afa77cee7853944b0119a3
felipepg22 <felipepg@uniplaclages.edu.br>
2026-03-01 18:13:41 -0300

    Creater NotificationCreator and added to EmailNotificationCreator

---
+++ FactoryMethod/src/NotificationCreators/EmailNotificationCreator.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoryMethod.NotificationCreators;
+
+using FactoryMethod.Notifications;
+
+public class EmailNotificationCreator : NotificationCreator
+{
+    public override INotification CreateNotification() =>
+         new EmailNotification();
+}
---
+++ FactoryMethod/src/NotificationCreators/NotificationCreator.cs
@@ -0,0 +1,14 @@
+﻿using FactoryMethod.Notifications;
+
+namespace FactoryMethod.NotificationCreators;
+
+public abstract class NotificationCreator
+{
+    public abstract INotification CreateNotification();
+
+    public void Notify()
+    {
+        var notification = CreateNotification();
+        notification.Send();
+    }
+}
